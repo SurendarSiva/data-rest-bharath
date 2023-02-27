bf997c18aa46cbe5fb59dd9c701bd2467dd4c5a1
SurendarSiva <surendarkrt@gmail.com>
2023-02-27 10:57:14 +0530

    Initial commit

---
+++ src/test/java/io/suren/datarest/DatarestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.suren.datarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatarestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
